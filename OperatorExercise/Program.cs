6c7fbae45190dc7993f472bc08189e6686a18f14
Eber27 <eberfreire@gmail.com>
2021-02-19 19:46:08 -0600

    Operators

---
+++ OperatorExercise/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace OperatorsExercise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a;
+            int b;
+
+            a = 17;
+            b = 4;
+
+            int sum = a + b;
+            int quotient = a / b;
+            int remainder = a % b;
+
+            Console.WriteLine($"{a}/{b} is {quotient} remainder {remainder} and sum is {sum}:");
+            Console.Write("Which is the radius of the circle:");
+            double radius = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine(AreaOfCircle(radius));
+        }
+
+        static double AreaOfCircle (double radius)
+        {
+            return Math.PI * radius * radius;
+        }
+    }
+}
